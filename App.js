eaa664732b0f90b8f0b12b2c212724f8a60a3203
Shenriquez <shenriquez2021@alu.uct.cl>
2024-09-20 20:41:32 -0300

    Login funcional con datos de prueba
    creacion del login funcionando, flata conectar con base de datos, recuperacion de contraseña con mensajes de error y envio sin conectar a backend

---
+++ App.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigator from './src/navigation/StackNavigator';  // Importa el stack de navegación
+import { UserProvider } from './src/contexts/UserContext';      // Importa el contexto de usuario
+import { ClientProvider } from './src/contexts/ClientContext'; // Importa el contexto de cliente
+
+export default function App() {
+  return (
+    
+    <UserProvider>
+      <ClientProvider>
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      </ClientProvider>
+    </UserProvider>
+  );
+}
